d04734448a7ff92eacba216c149a2b2ed1523280
Leo <leolazzarini101@gmail.com>
2021-09-07 17:14:46 -0700

    Getting all relevant information other than income

--- src/scraper/indeedScraper.ts
+++ src/scraper/indeedScraper.ts
@@ -1,3 +1,4 @@
+import { query } from 'express';
 import * as puppeteer from 'puppeteer';
 
 export function scrape() {
@@ -9,16 +10,64 @@ export function scrape() {
         'https://www.indeed.com/jobs?q=software%20developer&l=Washington%20State&jt=parttime',
       );
 
+      //       let urls = await page.evaluate(() => {
+      //         let results = [];
+      //         let matches = document.querySelectorAll('td#resultsCol');
+      //         matches.forEach((item) => {
+      //           results.push({
+      //             title: document.querySelector('h2.jobTitle > span').textContent,
+      //             companyName: document.querySelector('span.companyName').textContent,
+      //             companyLocation: document.querySelector('div.companyLocation')
+      //               .textContent,
+      //             jobDescription: document
+      //               .querySelector('div.job-snippet')
+      //               .textContent.trim()
+      //               .replace(/\n|\r/g, ''),
+      //             income: document.querySelector('span.salary-snippet').textContent,
+      //             link: document.querySelector('a').href,
+      //           });
+      //         });
+      //         return results;
+      //       });
+      //       browser.close();
+      //       console.log(urls.length);
+      //       return resolve(urls);
+      //     } catch (e) {
+      //       return reject(e);
+      //     }
+      //   });
+      // }
+      // scrape().then(console.log).catch(console.error);
+
+      //             companyLocation: document.querySelector('div.companyLocation')
+      //               .textContent,
+      //             jobDescription: document
+      //               .querySelector('div.job-snippet')
+      //               .textContent.trim()
+      //               .replace(/\n|\r/g, ''),
+      //             income: document.querySelector('span.salary-snippet').textContent,
+      //             link: document.querySelector('a').href,
+
       let urls = await page.evaluate(() => {
-        let results = [];
-        let container = document.querySelector('#resultsCol');
-        let matches = container.querySelectorAll('h2.jobTitle > span');
-        matches.forEach((item) => {
-          results.push({
-            text: item.innerHTML.trim(),
-          });
-        });
-        return results;
+        const titleNodeList = document.querySelectorAll('h2.jobTitle > span');
+        const companyName = document.querySelectorAll('span.companyName');
+        const companyLocation = document.querySelectorAll(
+          'div.companyLocation',
+        );
+        const jobDescription = document.querySelectorAll('div.job-snippet');
+        const income = document.querySelectorAll('span.salary-snippet');
+        //const link = document.querySelectorAll('a');
+        var jobArray = [];
+        for (var i = 0; i < titleNodeList.length; i++) {
+          jobArray[i] = {
+            title: titleNodeList[i].textContent.trim(),
+            companyName: companyName[i].textContent.trim(),
+            companyLocation: companyLocation[i].textContent.trim(),
+            jobDescription: jobDescription[i].textContent.trim(),
+            //income: income[i].textContent.trim()
+          };
+        }
+        return jobArray;
       });
       browser.close();
       return resolve(urls);
