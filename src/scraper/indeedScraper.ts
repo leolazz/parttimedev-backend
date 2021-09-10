c560e71295efe56717d6bf35c7bbe408b919d708
Leo <leolazzarini101@gmail.com>
2021-09-10 16:50:17 -0700

    Colllecting all relevant information

--- src/scraper/indeedScraper.ts
+++ src/scraper/indeedScraper.ts
@@ -1,4 +1,3 @@
-import { query } from 'express';
 import * as puppeteer from 'puppeteer';
 
 export function scrape() {
@@ -8,63 +7,42 @@ export function scrape() {
       const page = await browser.newPage();
       await page.goto(
         'https://www.indeed.com/jobs?q=software%20developer&l=Washington%20State&jt=parttime',
+        { waitUntil: 'domcontentloaded' },
       );
-
-      //       let urls = await page.evaluate(() => {
-      //         let results = [];
-      //         let matches = document.querySelectorAll('td#resultsCol');
-      //         matches.forEach((item) => {
-      //           results.push({
-      //             title: document.querySelector('h2.jobTitle > span').textContent,
-      //             companyName: document.querySelector('span.companyName').textContent,
-      //             companyLocation: document.querySelector('div.companyLocation')
-      //               .textContent,
-      //             jobDescription: document
-      //               .querySelector('div.job-snippet')
-      //               .textContent.trim()
-      //               .replace(/\n|\r/g, ''),
-      //             income: document.querySelector('span.salary-snippet').textContent,
-      //             link: document.querySelector('a').href,
-      //           });
-      //         });
-      //         return results;
-      //       });
-      //       browser.close();
-      //       console.log(urls.length);
-      //       return resolve(urls);
-      //     } catch (e) {
-      //       return reject(e);
-      //     }
-      //   });
-      // }
-      // scrape().then(console.log).catch(console.error);
-
-      //             companyLocation: document.querySelector('div.companyLocation')
-      //               .textContent,
-      //             jobDescription: document
-      //               .querySelector('div.job-snippet')
-      //               .textContent.trim()
-      //               .replace(/\n|\r/g, ''),
-      //             income: document.querySelector('span.salary-snippet').textContent,
-      //             link: document.querySelector('a').href,
-
+      await page.waitForSelector('span.salary-snippet');
       let urls = await page.evaluate(() => {
-        const titleNodeList = document.querySelectorAll('h2.jobTitle > span');
+        const container = document.querySelector('#resultsCol');
+        const linksNodeList: NodeListOf<HTMLAnchorElement> =
+          container.querySelectorAll('a[id^="job_"], a[id^="sj_"]');
+        const title = document.querySelectorAll('h2.jobTitle > span');
         const companyName = document.querySelectorAll('span.companyName');
         const companyLocation = document.querySelectorAll(
           'div.companyLocation',
         );
+        const incomeCondition = document.querySelectorAll('td.resultContent');
         const jobDescription = document.querySelectorAll('div.job-snippet');
         const income = document.querySelectorAll('span.salary-snippet');
-        //const link = document.querySelectorAll('a');
-        var jobArray = [];
-        for (var i = 0; i < titleNodeList.length; i++) {
+        let checkedIncome = [];
+        let incomeCounter = 0;
+        for (let i = 0; i < linksNodeList.length; i++) {
+          if (incomeCondition[i].childElementCount === 4) {
+            checkedIncome.push(income[incomeCounter].textContent);
+            incomeCounter++;
+          } else {
+            checkedIncome.push('Not Available');
+          }
+        }
+        let jobArray = [];
+        for (let i = 0; i < linksNodeList.length; i++) {
           jobArray[i] = {
-            title: titleNodeList[i].textContent.trim(),
+            title: title[i].textContent.trim(),
             companyName: companyName[i].textContent.trim(),
             companyLocation: companyLocation[i].textContent.trim(),
-            jobDescription: jobDescription[i].textContent.trim(),
-            //income: income[i].textContent.trim()
+            jobDescription: jobDescription[i].textContent
+              .trim()
+              .replace(/\n|\r/g, ''),
+            income: checkedIncome[i],
+            link: linksNodeList[i].href,
           };
         }
         return jobArray;
