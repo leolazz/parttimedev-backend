a63ab543a7e14a3730cee8bb034be258c1e7250e
leolazz <leolazzarini101@gmail.com>
2022-01-04 12:32:40 -0800

    port changed to 8080 for osx

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(5000);
+  await app.listen(8080);
 }
 bootstrap();
